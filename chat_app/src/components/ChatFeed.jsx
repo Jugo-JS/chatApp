9f216e491b0944feaff5b7d3ead1dc1eb59bc0bb
Jugo-JS <jugoslavsubotic@gmail.com>
2021-06-17 14:37:10 +0200

    adding custom chat feed

--- chat_app/src/components/ChatFeed.jsx
+++ chat_app/src/components/ChatFeed.jsx
@@ -18,7 +18,7 @@ const ChatFeed = (props) => {
             return (
                 <div key={`msg_${index}`} style={{ width:'100%' }}>
                     <div className='message-block'>
-                        {isMyMessage ? <MyMessage /> : <TheirMessage />}
+                        {isMyMessage ? <MyMessage message={message} /> : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>}
                     </div>
                     <div className='read-receipts' style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
                         read-receipts
@@ -28,11 +28,22 @@ const ChatFeed = (props) => {
         })
     } 
 
-    renderMessages()
+
+    if(!chat) return 'Loading...'
 
     return (
-        <div>
-            ChatFeed
+        <div className='chat-feed'>
+            <div className='chat-title-container'>
+                <div className='chat-title'>{chat.title}</div>
+                <div className='chat-subtitle'>
+                    {chat.map((person) => ` ${person.person.userName}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height:'100px' }} />
+            <div className='message-form-container'>
+                <MessageForm {...props} chatID={activeChat} />
+            </div>
         </div>
     )
 }
